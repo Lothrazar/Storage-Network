9f46f9ed1c886a66887ee473a1e0de897fb29f5a
Metalit <48568911+Metalit@users.noreply.github.com>
2023-09-06 18:14:30 -0500

    Facades

--- src/main/java/mrriegel/storagenetwork/block/cable/TesrCable.java
+++ src/main/java/mrriegel/storagenetwork/block/cable/TesrCable.java
@@ -2,13 +2,25 @@ package mrriegel.storagenetwork.block.cable;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -28,6 +40,36 @@ public class TesrCable extends TileEntitySpecialRenderer<TileCable> {
     model = new ModelCable();
   }
 
+  public void renderFacadeBlock(IBlockState block, World world, BlockPos tePos, double x, double y, double z) {
+    Tessellator tessellator = Tessellator.getInstance();
+    BufferBuilder buffer = tessellator.getBuffer();
+    this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+    RenderHelper.disableStandardItemLighting();
+    GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+    GlStateManager.enableBlend();
+    GlStateManager.disableCull();
+
+    if (Minecraft.isAmbientOcclusionEnabled()) {
+      GlStateManager.shadeModel(GL11.GL_SMOOTH);
+    } else {
+      GlStateManager.shadeModel(GL11.GL_FLAT);
+    }
+
+    buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+
+    IBlockAccess renderAccess = MinecraftForgeClient.getRegionRenderCache(world, tePos);
+    buffer.setTranslation(x - tePos.getX(), y - tePos.getY(), z - tePos.getZ());
+
+    BlockRendererDispatcher dispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+    dispatcher.renderBlock(block, tePos, new BlockAccessFacade(renderAccess), buffer);
+
+    buffer.setTranslation(0, 0, 0);
+
+    tessellator.draw();
+
+    RenderHelper.enableStandardItemLighting();
+  }
+
   @Override
   public void render(TileCable te, double x, double y, double z, float partialTicks, int destroyStage, float partial) {
     if (te == null) {
@@ -41,6 +83,11 @@ public class TesrCable extends TileEntitySpecialRenderer<TileCable> {
     IExtendedBlockState extendedBlockState = (IExtendedBlockState) blockstate.getBlock().getExtendedState(blockstate, te.getWorld(), te.getPos());
     UnlistedPropertyBlockNeighbors.BlockNeighbors neighbors = extendedBlockState.getValue(BlockCable.BLOCK_NEIGHBORS);
     if (neighbors == null) {
+      return;
+    }
+    IBlockState facadeState = te.facadeState;
+    if (facadeState != null) {
+      renderFacadeBlock(facadeState, te.getWorld(), te.getPos(), x, y, z);
       return;
     }
     GlStateManager.pushMatrix();
