dd8b17c05c2575fce0917e5abbd68de65a4041e9
ArtByteFilip <artbytefilip@protonmail.com>
2025-08-19 18:30:08 +0200

    you no longer need to hold it in your hand to switch it

--- src/main/java/com/lothrazar/storagenetwork/network/KeybindCollectorToggleMessage.java
+++ src/main/java/com/lothrazar/storagenetwork/network/KeybindCollectorToggleMessage.java
@@ -1,10 +1,9 @@
 package com.lothrazar.storagenetwork.network;
 
 import com.lothrazar.storagenetwork.item.ItemCollector;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkEvent;
 
@@ -12,20 +11,40 @@ import java.util.function.Supplier;
 
 public class KeybindCollectorToggleMessage {
 
-    public KeybindCollectorToggleMessage() {}
+    public KeybindCollectorToggleMessage() {
+    }
 
     public static void handle(KeybindCollectorToggleMessage msg, Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
             ServerPlayer player = ctx.get().getSender();
             if (player != null) {
-                ItemStack held = player.getMainHandItem();
-                if (held.getItem() instanceof ItemCollector collector) {
-                    collector.toggleEnabled(held, player);
+                Inventory inv = player.getInventory();
+
+                for (ItemStack stack : inv.items) {
+                    if (stack.getItem() instanceof ItemCollector collector) {
+                        collector.toggleEnabled(stack, player);
+                        return;
+                    }
+                }
+
+                for (ItemStack stack : inv.armor) {
+                    if (stack.getItem() instanceof ItemCollector collector) {
+                        collector.toggleEnabled(stack, player);
+                        return;
+                    }
+                }
+
+                for (ItemStack stack : inv.offhand) {
+                    if (stack.getItem() instanceof ItemCollector collector) {
+                        collector.toggleEnabled(stack, player);
+                        return;
+                    }
                 }
             }
         });
         ctx.get().setPacketHandled(true);
     }
+
 
     public void encode(FriendlyByteBuf friendlyByteBuf) {
     }
